35632a36ebf753965f57972b14ff7e99d78fe623
boonsim <boonsim@kku.ac.th>
2021-03-03 14:58:36 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from botnoi import cv
+import requests
+import joblib
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World !'
+
+@app.route('/prediction')
+def classifier():
+    img_url = request.values['p_image_url']
+    a = cv.image(img_url)
+    feat = a.getresnet50()
+    modFile = 'CDmodel.mod'
+    model = pickle.load(open(modFile,'rb'))
+    
+    #Predict classes with LinearSVC
+    prediction = model.predict([feat])
+    print(feat.shape)
+    result = {'img_url': img_url, 'prediction': prediction[0]}
+    return jsonify(result)
+
+if __name__=='__main__':
+    app.run(debug=True)
